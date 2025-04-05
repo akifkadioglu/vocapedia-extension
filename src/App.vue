7bd1b46469cea9b470fdc2d46f26b930f1a275a6
Akif <akif.kadioglu.28@gmail.com>
2025-04-05 13:35:36 +0300

    feat: initialize Vocapedia Extension with Vue and Tailwind CSS
    - Added manifest.json for Chrome extension configuration.
    - Created main App.vue component with a header and welcome section.
    - Included base CSS file for Tailwind CSS integration.
    - Added logo image assets in PNG and SVG formats.
    - Set up main.js to bootstrap the Vue application.
    - Configured Vite with Vue and Tailwind CSS plugins.
    - Created an empty main.css file for future styles.

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="w-full">
+    <img alt="Vocapedia logo" src="./assets/logo.png" width="125" height="125" />
+
+    <div class="wrapper">
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<script setup>
+</script>
